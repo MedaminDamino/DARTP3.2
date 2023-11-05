e1975db782d2189d8dfdaf5e9c772eeeb080fec7
MedaminDamino <medamin.abdelkafi753@gmail.com>
2023-11-05 19:11:41 +0100

    TP3.2

---
+++ src/server/ClientProcess.java
@@ -0,0 +1,48 @@
+package server;
+
+import java.io.*;
+import java.net.Socket;
+
+class ClientProcess extends Thread {
+    Socket clientSocket;
+
+    public ClientProcess(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println("Je un client "+clientSocket.getRemoteSocketAddress());
+
+            InputStream is = clientSocket.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            Operation op = (Operation)ois.readObject();
+
+            int p1 = Integer.parseInt(op.getP1());
+            String p2 = op.getO();
+            int p3 = Integer.parseInt(op.getP2());
+
+            float nb=0;
+
+            switch(p2) {
+                case "+": nb = p1+p3;
+                    break;
+                case "-": nb = p1-p3;
+                    break;
+                case "*": nb = p1*p3;
+                    break;
+                case "/": nb = p1/p3;
+                    break;
+            }
+            op.setRes(nb);
+            OutputStream os = clientSocket.getOutputStream();
+            ObjectOutputStream ooos = new ObjectOutputStream(os);
+            ooos.writeObject(op);
+
+            clientSocket.close();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
